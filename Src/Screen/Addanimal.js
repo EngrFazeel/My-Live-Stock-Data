7e3f46c8bb4fdde8e586eff199c89db5d91b2557
Fazeel Ur Rehman <142094674+EngrFazeel@users.noreply.github.com>
2026-04-26 23:23:55 +0500

    fazeel

--- Src/Screen/Addanimal.js
+++ Src/Screen/Addanimal.js
@@ -13,7 +13,6 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Dropdown } from 'react-native-element-dropdown';
 import DatePicker from 'react-native-date-picker';
-import Home from './Home';
 
 export default class Addanimal extends Component {
     constructor(props) {
