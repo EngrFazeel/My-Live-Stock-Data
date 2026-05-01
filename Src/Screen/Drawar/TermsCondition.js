6942a20ff19580410d2da8c43e0a2feb382376e2
amna8756 <amnar8756@gmail.com>
2026-05-01 22:48:45 +0500

    Fazeel

--- Src/Screen/Drawar/TermsCondition.js
+++ Src/Screen/Drawar/TermsCondition.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ScrollView,
-  SafeAreaView,
-  StatusBar,
-  TouchableOpacity
-} from 'react-native';
+import {View,Text, StyleSheet, ScrollView, SafeAreaView, StatusBar, TouchableOpacity} from 'react-native';
 import { color } from '../../Color';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
